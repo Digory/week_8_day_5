ddd4c82187430efed37e1fd29cc6ff7af10a1508
Digory <digory1@googlemail.com>
2018-08-17 19:44:01 +0100

    All Studio tests pass

--- src/main/java/models/Film.java
+++ src/main/java/models/Film.java
@@ -1,4 +1,65 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Film {
+
+    private String title;
+    private FilmGenreType genre;
+    private Studio studio;
+    private Director director;
+    private List<Actor> actors;
+
+    public Film(String title, FilmGenreType genre, Studio studio, Director director) {
+        this.title = title;
+        this.genre = genre;
+        this.studio = studio;
+        this.director = director;
+        actors = new ArrayList<Actor>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public FilmGenreType getGenre() {
+        return genre;
+    }
+
+    public void setGenre(FilmGenreType genre) {
+        this.genre = genre;
+    }
+
+    public Studio getStudio() {
+        return studio;
+    }
+
+    public void setStudio(Studio studio) {
+        this.studio = studio;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+
+    public void setDirector(Director director) {
+        this.director = director;
+    }
+
+    public List<Actor> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<Actor> actors) {
+        this.actors = actors;
+    }
+
+    public void addActor(Actor actor){
+        actors.add(actor);
+    }
 }
--- src/test/java/TestStudio.java
+++ src/test/java/TestStudio.java
@@ -1,2 +1,48 @@
+import models.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 public class TestStudio {
+
+    private Studio studio;
+    private Director director;
+    private Film film;
+
+
+    @Before
+    public void before(){
+        studio = new Studio("Nice Films");
+        director = new Director("Digory", studio);
+        film = new Film("Mark's marvellous rabbits", FilmGenreType.JUST_WEIRD, studio, director);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Nice Films", studio.getName());
+    }
+
+    @Test
+    public void directorListEmptyOnInitialization(){
+        assertEquals(0, studio.getDirectors().size());
+    }
+
+    @Test
+    public void canAddDirector(){
+        studio.addDirector(director);
+        assertEquals(1, studio.getDirectors().size());
+    }
+
+    @Test
+    public void filmListEmptyOnInitialization(){
+        assertEquals(0, studio.getFilms().size());
+    }
+
+    @Test
+    public void canAddFilm(){
+        studio.addFilm(film);
+        assertEquals(1, studio.getFilms().size());
+    }
+
 }
